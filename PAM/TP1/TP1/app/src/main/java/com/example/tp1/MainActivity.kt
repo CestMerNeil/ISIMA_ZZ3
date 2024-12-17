bf5bd9939b7ea6039eb7acd5f7a57951caf2393e
Neil <62321203+CestMerNeil@users.noreply.github.com>
2024-12-17 17:22:32 +0100

    update

---
+++ PAM/TP1/TP1/app/src/main/java/com/example/tp1/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.tp1
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tp1.ui.theme.TP1Theme
+import com.example.tp1.ui.theme.Pink40
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.ui.platform.LocalContext
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TP1Theme {
+                TP1_Activity1()
+            }
+        }
+    }
+}
+
+@Composable
+fun TP1_Activity1() {
+    val message = remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = {
+                Toast.makeText(context, "Never use Android! PLEASE!", Toast.LENGTH_SHORT).show()
+                message.value = "Android"
+            }, colors = ButtonDefaults.buttonColors(containerColor = Pink40)) {
+                Text("Click me!")
+            }
+            if (message.value.isNotEmpty()) {
+                Greeting(message.value)
+            }
+            TextField(
+                value = message.value,
+                onValueChange = { message.value = it },
+                label = { Text("Enter your name") }
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Never use Android! PLEASE! $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TP1Theme {
+        Greeting("Android", Modifier.padding(16.dp))
+    }
+}
